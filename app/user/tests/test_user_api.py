96df2023ae173471cbce52430c8379a585062e4d
vlakaswb <vlakasofficial@gmail.com>
2023-03-23 19:07:11 +0700

    added func manage user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -116,7 +116,7 @@ class PrivateUserApiTests(TestCase):
         self.user = create_user(
             email='test@example.com',
             password='testpass123',
-            name='Test Name'
+            name='Test Name',
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
